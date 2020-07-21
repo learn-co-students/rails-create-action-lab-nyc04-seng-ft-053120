de1283a0bd7474ee58714b96830c337d1dca30fb
Rouan Thompson <rouan.thompson@gmail.com>
2020-07-21 00:07:49 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+Student.create([
+    {
+        first_name: "Tim",
+        last_name: "King"
+    },
+    {
+        first_name: "Carla",
+        last_name: "Chagata"
+    }]
+)
